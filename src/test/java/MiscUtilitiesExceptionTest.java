2f1cb5507138e9d25247ba7c62900247339ab50a
omatkar7 <matkar.omkar90@gmail.com>
2022-08-02 23:03:42 +0530

    Cosmetic Changes

--- src/test/java/MiscUtilitiesExceptionTest.java
+++ src/test/java/MiscUtilitiesExceptionTest.java
@@ -1,11 +1,13 @@
 import org.example.LoginException;
 import org.example.MiscUtilities;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MiscUtilitiesExceptionTest {
 
     @Test
+    @DisplayName("Verify Custom Exception Handling")
     void testLoginException() {
         System.out.println("Test Exception Handling");
         Exception exception = Assertions.assertThrows(LoginException.class, () -> MiscUtilities.doLogin(null,null));
